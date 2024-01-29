0424205184ff2c7850179aa478b3fa5d5642deb9
Madhur Mittal <madhurmittal55@gmail.com>
2024-01-29 17:56:47 +0530

    changes

--- frontend/src/app/anonChat/page.tsx
+++ frontend/src/app/anonChat/page.tsx
@@ -4,12 +4,32 @@ import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import AES from 'crypto-js/aes';
+import encBase64 from 'crypto-js/enc-base64';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
+const CRYPTO_KEY = process.env.CRYPTO_KEY || '';
 const page = () => {
     const [userName, setUserName] = useState<string>('');
+
+    const handleEnterChat = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    ): void => {
+        e.preventDefault();
+        const encryptName = AES.encrypt(
+            userName,
+            CRYPTO_KEY,
+        ).ciphertext.toString(encBase64);
+
+        const encryptURL = AES.encrypt(
+            Math.floor(100000 + Math.random() * 900000).toString(),
+            CRYPTO_KEY,
+        ).ciphertext.toString(encBase64);
+
+        console.log(encryptName);
+    };
 
     return (
         <MaxWidthWrapper>
@@ -47,19 +67,24 @@ const page = () => {
                                         onKeyDown={(e) => {
                                             e.key === 'Enter'
                                                 ? userName.trim() !== ''
-                                                    ? null
+                                                    ? handleEnterChat
                                                     : null
                                                 : null;
                                         }}
                                     />
-                                    <Link href={'/anonChat/go'} className="">
-                                        <Button
-                                            type="submit"
-                                            className="w-full"
-                                        >
-                                            Join
-                                        </Button>
-                                    </Link>
+                                    {/* <Link href={'/anonChat/go'} className=""> */}
+                                    <Button
+                                        type="submit"
+                                        className="w-full"
+                                        onClick={(e) => {
+                                            return userName.trim() !== ''
+                                                ? handleEnterChat(e)
+                                                : null;
+                                        }}
+                                    >
+                                        Join
+                                    </Button>
+                                    {/* </Link> */}
                                 </div>
                             </form>
                         </div>
